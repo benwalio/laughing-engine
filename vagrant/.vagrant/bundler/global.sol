71ba730385a707f527a0a203df9997d6857a98bc
ben <ben.wallace@protonmail.ch>
2023-01-07 13:56:01 -0500

    1673117761-autosave

---
+++ vagrant/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["racc",["~> 1.4"]],["nokogiri",[">= 0"]],["vagrant-parallels",["= 2.2.6"]],["vagrant-vmware-desktop",["= 3.0.1"]]],"checksum":"56f5262ec0fe728e52be240854e5db287a79f8350e599840293456276bca9f8f","vagrant_version":"2.3.4"}
---
+++ vagrant/triggers/Vagrant_trigger_after_create.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+# sed -i_bak -e "/10\.69\.69\./d" ~/.ssh/known_hosts
